447a712d16a353165f863babae873548315d9ad9
ken-sevenseas <inakanookaki@icloud.com>
2022-07-14 12:29:15 +0900

    HoT: initial revision

---
+++ Head.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Head {
+    public static void main(String[] args) {
+        int h = 0;
+        int t = 0;
+        Random rand = new Random();
+        System.out.println("Tossing a coin...");
+        for (int i = 0; i < 5; i++) {
+            int num = rand.nextInt(2);
+            if (num == 0) {
+                h++;
+                System.out.println("Round" + i + ": Heads");
+            } else if (num == 1) {
+                t++;
+                System.out.println("Round" + i + ": Tails");
+            }
+        }
+        System.out.println("Heads:" + h + ", Tails:" + t);
+    }
+
+}
